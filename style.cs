035203ae1ab24731c83f1ae69d73540fdc94b594
Moonlightcrush12 <87609552+Moonlightcrush12@users.noreply.github.com>
2021-07-24 19:06:19 +0530

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,19 @@
+<body>{
+    text-allign: center;
+    background color:lightblue; 
+}
+span{
+    margin-top:15px;
+}
+#Hi1{
+    width:150px;
+}
+#Hi2{
+    width:150px;
+}
+#Hi3{
+    width:150px;
+}
+#Hi4{
+    width:150px;
+}
